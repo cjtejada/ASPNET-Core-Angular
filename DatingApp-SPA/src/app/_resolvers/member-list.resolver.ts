a3b0d14d108249adedd950417e9a3ecdda3d482b
Claudio <cjtejada@outlook.com>
2020-01-24 17:34:36 -0600

    Section 14

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -9,20 +9,23 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class MemberListResolver implements Resolve<User>{
 
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) { }
+  pageNumber = 1;
+  pageSize = 5;
 
-    resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUsers().pipe(
+  constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) { }
 
-            catchError(error => {
-                this.alertify.error('Problems retriving data');
-                this.router.navigate(['/home']);
-                return of(null);
-            })
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
+    return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
 
-        );
+      catchError(error => {
+        this.alertify.error('Problems retriving data');
+        this.router.navigate(['/home']);
+        return of(null);
+      })
 
-    }
+    );
+
+  }
 
 }
 
--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../../_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -10,23 +11,51 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-    users: User[];
+  users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
+  userParams: any = {};
+  pagination: Pagination
 
-    constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
-    ngOnInit() {
-        this.route.data.subscribe(data => {
-            this.users = data['users'];
-        });
-    }
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
-    //loadUsers() {
-    //    this.userService.getUsers().subscribe((users: User[]) => {
-    //        this.users = users;
-    //    }, error => {
-    //        this.alertify.error(error);
-    //    });
-    //}
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    console.log(this.pagination.itemsPerPage);
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams).subscribe(
+      (res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertify.error(error);
+      }
+    );
+  }
+
 }
 
 
